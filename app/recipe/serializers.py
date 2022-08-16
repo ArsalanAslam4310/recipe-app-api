5ea9cfb0a5d6f3776402027938b2d12afee6a5c0
ArsalanAslam4310 <arsalanaslam4310@gmail.com>
2022-08-16 16:19:56 +0500

    Build ingredients API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,12 +31,12 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe."""
     tags = TagSerializer(many=True, required=False)
-    ingredient = IngredientSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes',
-                  'price', 'link', 'tags', 'ingredient']
+                  'price', 'link', 'tags', 'ingredients']
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -72,9 +72,13 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
